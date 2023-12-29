f5abdcb3cfb17a3f24c052da12553605c1b77b7f
MOROLAKEOJO <153183963+MOROLAKEOJO@users.noreply.github.com>
2023-12-29 19:43:54 +0100

    Update sales record data analysis.sql

--- sales record data analysis.sql
+++ sales record data analysis.sql
@@ -2,55 +2,70 @@ select * from `sale record`;
 use `sale record`;
 
 -- 1 What is our best selling product? 
-create view `best selling products` as
-SELECT  (price * quantity) revenue, product from `sale record`
-order by (price * quantity) desc;
+create view `best selling product` as
+select product, total_revenue
+from(SELECT product, (price * quantity) total_revenue 
+from `sale record`
+group by product, TOTAL_REVENUE) as subquery
+order by total_revenue desc;
 
- -- 2 What is our total revenue?
+
+ -- 2 What is our total revenue
  create view `total revenue` as
   SELECT  sum(price * quantity)as revenue from `sale record`;
 
+
 --  3) What is the revenue breakdown by payment method? */
 create view `payment method by revenue` as
-select `payment method`, sum(price* quantity) revenue
+select `payment method`, sum(price* quantity) total_revenue
 from `sale record`
 group by `payment method`;
 
+
 -- 4) What payment method the customers prefer
-create view `most preferred payment methods` as 
-select (count(`payment method`)) numbers, `Payment Method`
+create view `most preferred payment method` as
+select `Payment Method`, (count(`payment method`)) count
 from `sale record`
-group by `payment method` 
+group by `payment method`
 order by (count(`payment method`)) desc;
 
+
 -- 5) Display the trend of sale over days of the week
-create view `trend of sales over the days of the week` as 
-select dayname(date), sum(price * quantity)
+create view `trends of sales over the days of the week` as
+select dayname(date),product, sum(price * quantity) total_revenue
 from `sale record`
-group by dayname(date);
+group by dayname(date), product;
+
 
 -- 6) What month has the highest revenue
-create view `Month with revenues` as
-SELECT monthname(date), (price * quantity) as revenue from `sale record`
-order by (price * quantity) desc;
+create view `Months with revenues generated` as
+select `date`, total_revenue
+from(SELECT monthname(`date`) `date`, sum(price * quantity) as total_revenue 
+from `sale record`
+group by monthname(`date`)) as subquery
+order by total_revenue desc;
+
 
 -- 7) What is the breakdown of revenue by manager
 create view `Revenue by Manager` as
-select distinct(`manager`), sum(price* quantity) revenue
+select distinct(manager), total_revenue
+from (select regexp_replace(manager, '\s+', ' ') manager, sum(price * quantity) total_revenue
 from `sale record`
-group by `manager`;
+group by manager ) subquery
+order by total_revenue desc;
 
 
 use `sale record`;
 -- 8 manager by city
 create view `manager by city` as
-SELECT  distinct( trim(replace(manager, "  "," "))) manager, city
- from `sale record`;
+SELECT city, count(manager) count 
+from `sale record`
+group by city ;
 
 
 -- 9 MOST EXPENSIVE PRODUCT
 create view `five most expensive productss` as
-SELECT product, PRICE
+SELECT distinct(product), PRICE
 from `sale record`
 order by 2 desc
 limit 0,5;
